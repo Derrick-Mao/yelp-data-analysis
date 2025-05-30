5b03d98dc0ac29823e26c24c3838afb9f43b7fc5
Derrick Mao <derrickmao03@gmail.com>
2025-05-30 14:52:30 -0700

    move Header to layout

--- src/app/anomaly/page.tsx
+++ src/app/anomaly/page.tsx
@@ -1,10 +1,6 @@
-import Header from "../components/header";
-
 const AnomalyPage = () => {
   return (
     <div>
-      <Header />
-
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <h2 className="text-xl font-semibold text-white mb-4">
           Anomalies Analysis
--- src/app/behavior/page.tsx
+++ src/app/behavior/page.tsx
@@ -1,10 +1,6 @@
-import Header from "../components/header";
-
 const UserBehavior = () => {
   return (
     <div>
-      <Header />
-
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <h2 className="text-xl font-semibold text-white mb-4">
           Elite vs Non-Elite
